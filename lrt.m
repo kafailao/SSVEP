2b602a28c1172b529b13dfc0c8f99a504f23be6f
KafaiLao <kflao.umac@gmail.com>
2016-09-04 23:23:26 +0800

    Rename

--- lrt.m
+++ lrt.m
@@ -1,10 +1,10 @@
-function p = lrt(Xnew,Xtemplate)
+function score = lrt(Xnew,Xtemplate)
 % Xnew [samples,channels]
 % Xtemplate [freq,samples,channels]
 if ismatrix(Xtemplate)
     Xtemplate = reshape(Xtemplate,[1 size(Xtemplate)]);
 end
-p = zeros(size(Xtemplate,1),1);
+score = zeros(size(Xtemplate,1),1);
 Nsample = size(Xnew,1);
 Xnew = normalizeSignal(Xnew,1);
 Xtemplate = normalizeSignal(Xtemplate,2);
@@ -17,7 +17,7 @@ for freq = 1:size(Xtemplate,1)
     C = 1/Nsample*(intSignal'*intSignal);
     Cx = C(1:dimOne,1:dimOne);
     Cy = C(dimOne+1:dimA,dimOne+1:dimA);
-    p(freq) = 1 - (det(C)/(det(Cx)*det(Cy)+10e-9))^(1/size(Xtemplate,3));
+    score(freq) = 1 - (det(C)/(det(Cx)*det(Cy)+10e-9))^(1/size(Xtemplate,3));
 end
 end
 
