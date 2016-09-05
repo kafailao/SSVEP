a4d48d9764e388106a93d19319b1b5561b5df55c
KafaiLao <kflao.umac@gmail.com>
2016-09-06 00:35:27 +0800

    Modify fbcca

--- genFB.m
+++ genFB.m
@@ -1,19 +1,17 @@
 function filterBank = genFB(startFreq,endFreq,stopFreq,fsample,Nfilter,tol)
+% startFreq: minimum pass frequency (e.g., the minimum stimulus frequency)
+% endFreq: end frequency (e.g., the maximum stimulus frequency)
+% stopFreq: maximum frequency for the filter bank
+% fsample: sampling frequency
+% Nfilter: number of filter bank
+% tol: reduce/increase startFreq/stopFreq with tol
+
 startFreq = floor(startFreq);
 bandwidth = endFreq - startFreq;
 filterBank = cell(Nfilter,1);
 fs = fsample/2;
 for n = 1:Nfilter
-%     fst = startFreq + (n-1)*bandwidth;
-%     fed = stopFreq;
-%     [N,Wn] = cheb1ord([fst fed]/fs,[fst - tol fed + tol]/fs,3,60);
-%     [b,a] = cheby1(N,1,Wn);
-%     filterBank{n,1} = b;
-%     filterBank{n,2} = a;
     filterBank{n} = designfilt('bandpassiir','FilterOrder',10, ...
          'HalfPowerFrequency1',startFreq + (n-1)*bandwidth - tol,'HalfPowerFrequency2',stopFreq + tol, ...
          'SampleRate',fsample);
-%     designfilt('bandpassiir','FilterOrder',10, ...
-%         'PassbandFrequency1',startFreq + (n-1)*bandwidth - tol,'PassbandFrequency2',stopFreq + tol, ...
-%         'PassbandRipple',1,'SampleRate',fsample);
 end
