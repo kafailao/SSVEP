d8483afc61816ffa204e1c202183d0049ab3aff7
KafaiLao <kflao.umac@gmail.com>
2016-09-13 00:28:10 +0800

    Modifiy MSSA

--- mssa.m
+++ mssa.m
@@ -1,12 +1,14 @@
-function [lambda,V,PC] = mssa(X,M)
+function [lambda,V,PC,RC] = mssa(X,M)
 N = size(X,1);
 D = size(X,2);
 Y = zeros(N-M+1,D*M);
 % create time-delayed embedding of X
 X = normalizeSignal(X,1);
+
 for d = 1:D
+    pos = (d-1)*M+1:d*M;
     for n = 1:N-M+1        
-        Y(n,(d-1)*M+1:d*M) = X(n:n+M-1,d);
+        Y(n,pos) = X(n:n+M-1,d);
     end
 end
 
@@ -16,4 +18,17 @@ lambda = diag(S);      % extract the diagonal
 [lambda,ind]=sort(lambda,'descend'); % sort eigenvalues
 V = V(:,ind);             % and eigenvectors
 
-PC = Y*V;
+remain = ~(cumsum(lambda/sum(lambda)) > 0.9);
+if sum(remain) == 0, remain(1) = true; end;
+PC = Y*V(:,remain);
+
+RC = zeros(D,N,sum(remain));
+for d = 1:D
+    for m = 1:sum(remain)
+        buf = PC(:,m)*V((d-1)*M+1:d*M,m)'; % invert projection - first channel
+        buf = buf(end:-1:1,:);
+        for n = 1:N % anti-diagonal averaging
+            RC(d,n,m)=mean(diag(buf,-(N-M+1)+n));
+        end
+    end
+end;
--- mssa_ssvep.m
+++ mssa_ssvep.m
@@ -1,8 +1,13 @@
 function score = mssa_ssvep(Xnew,Xtemplate,M)
 score = zeros(size(Xtemplate,1),1);
 for freq = 1:size(Xtemplate,1)
+    % MSI style
     [lambda,~,~] = mssa([Xnew,squeeze(Xtemplate(freq,:,:))],M);
     nlabmda = lambda/sum(lambda);
     score(freq) = 1 + sum(bsxfun(@times,nlabmda,log(nlabmda)))/size(Xnew,2);
+    % CCA style
+%     [~,~,~,RC] = mssa(Xnew,M);
+%     [~,~,r] = canoncorr(squeeze(mean(RC,3))',squeeze(Xtemplate(freq,:,:)));
+%     score(freq) = r(1);
 end
 end
